aadf13a8582d71a796201484172068e9c877ac32
Bodhi-Spec <bodhiberoukhim@gmail.com>
2024-08-08 17:49:35 -0400

    Update VerticalShifts.R

--- VerticalShifts.R
+++ VerticalShifts.R
@@ -1,7 +1,7 @@
 #This file supports the Vertical Shift tests --> the functions (anyNegative() and belowBaseline()) that detects if the processed data is negative and below the baseline respectively
 #Both anyNegative() and belowBaseline() is called through the GetData() function which provides it with the data in the parameters
 
-#
+#Checks if any processed data falls below 0 within the metbolite range
 anyNegative<-function(spectra_data,met_data,misc_data,filename){
   negative_ppm<-spectra_data$ppm[spectra_data$ProcessedData<=0] #subset all the points which are negative
   negative_ppm<-negative_ppm[negative_ppm>1.8] #subsets all the points which are in the metabolite ppm range
@@ -14,7 +14,7 @@ anyNegative<-function(spectra_data,met_data,misc_data,filename){
 }
 
 
-#____Checls if any pafrt of the the graph is below the baseline
+#Checks if any processed data falls below the baseline within the metbolite range
 belowBaseline<-function(spectra_data,met_data,misc_data,filename){
   spectra_data<-subset(spectra_data,ppm>1.8) #only detects points in the metbaolite ppm range
   belowbaseline<-any(spectra_data$ProcessedData<=spectra_data$Background) #checks if any of the processed data dips below the baseline
