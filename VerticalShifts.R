018ef3b459138b9944e3336db87c2fd6199a5398
Bodhi-Spec <bodhiberoukhim@gmail.com>
2024-08-08 17:41:15 -0400

    Update and rename ArtifactNegative.R to VerticalShifts.R

---
+++ VerticalShifts.R
@@ -0,0 +1,19 @@
+#____ADD EXPLINATION
+anyNegative<-function(spectra_data,met_data,misc_data,filename){
+  negative_ppm<-spectra_data$ppm[spectra_data$ProcessedData<=0] #subset all the points which are negative
+  negative_ppm<-negative_ppm[negative_ppm>1.8] #subsets all the points which are in the metabolite ppm range
+  if (length(negative_ppm)>0){
+    anynegative<-TRUE
+  }else{
+    anynegative<-FALSE
+  }
+  return(list(anynegative=anynegative))
+}
+
+
+#____Checls if any pafrt of the the graph is below the baseline
+belowBaseline<-function(spectra_data,met_data,misc_data,filename){
+  spectra_data<-subset(spectra_data,ppm>1.8) #only detects points in the metbaolite ppm range
+  belowbaseline<-any(spectra_data$ProcessedData<=spectra_data$Background) #checks if any of the processed data dips below the baseline
+  return(list(belowbaseline=belowbaseline))
+}
